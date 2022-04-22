7fa4713f0588154be2a26a375048e368594cbed7
rsbettini <robson.bettini@hotmail.com>
2022-04-22 15:11:19 -0300

    Project ViaCep

---
+++ src/pages/CepSearch/index.tsx
@@ -0,0 +1,66 @@
+import './styles.css';
+
+import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+
+type FormData = {
+  cep: string;
+  test: string;
+}
+
+const CepSearch = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    cep:'',
+    test: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData( { ...formData, [name]:value } )
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+  return (
+    <div className="cep-search-container">
+      <h1 className="text-primary">Busca CEP</h1>
+      <div className="container search-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-container">
+            <input
+              type="text"
+              name= "cep"
+              value= {formData.cep}
+              className="search-input"
+              placeholder="CEP (somente números)"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="test"
+              value= {formData.test}
+              className="search-input"
+              placeholder="Test"
+              onChange={handleChange}
+            />
+            <button type="submit" className="btn btn-primary search-button">
+              Buscar
+            </button>
+          </div>
+        </form>
+
+        <ResultCard title="Logradouro" description="Lalala" />
+        <ResultCard title="Número" description="234" />
+
+      </div>
+    </div>
+  );
+};
+
+export default CepSearch;
